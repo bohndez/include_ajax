68aecd2c8a4cff041ab22dbf117f9e5193c171a9
globaldev <globaldev@iMac-de-globaldev.local>
2015-08-26 15:35:53 -0300

    Con comentarios

--- main.js
+++ main.js
@@ -12,13 +12,14 @@ function losInclude(archivo, donde){
 	//Si el estado es "preparado" y "ok"
 	conexion.onreadystatechange=function(){
 		if(conexion.readyState==4 && conexion.status==200){
+			//Selecciono el id del elemento donde voy a cargar el include
 			document.getElementById(donde).innerHTML=conexion.responseText;
 		}
 	}
-	//Recupero el valor de variable del archivo header.
+	//Recupero el contenido de los archivos a incluir.
 	conexion.open("GET", archivo, true);
 	conexion.send();
-	console.log(archivo);
 }
+//Llamo la función cada vez que quiera incluir un archivo.
 losInclude("header.html", 'header');
 losInclude("footer.html", 'footer');
